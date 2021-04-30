35f3c927ea2b39c1804d49a85c33b4151cf667b2
sierraskyewallace <sskwallace@gmail.com>
2021-04-30 15:23:52 -0700

    Done.

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,21 @@
+class SessionsController < ApplicationController
+    skip_before_action :auth_user, only: [:new, :create]
+
+    def new
+      @user = User.new
+    end
+  
+    def create
+      if @user = User.find_by(name: params[:user][:name])
+        session[:user_id] = @user.id
+        redirect_to user_path(@user)
+      else
+        render 'new'
+      end
+    end
+  
+    def destroy
+      reset_session
+      redirect_to root_path
+    end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,15 @@
 class User < ActiveRecord::Base
-end
+    has_secure_password
+    has_many :rides
+    has_many :attractions, through: :rides
+
+    def mood
+        unless admin
+          if nausea > happiness 
+             'sad'
+          else 
+             'happy'
+          end 
+        end
+      end
+   end
